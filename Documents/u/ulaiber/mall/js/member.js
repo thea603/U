2b922a6c6a7e36024fbb4951b2cbbfd7439120eb
thea603 <494752646@qq.com>
2015-03-14 11:17:35 +0800

    u

---
+++ Documents/u/ulaiber/mall/js/member.js
@@ -0,0 +1,26 @@
+
+	var content=document.getElementById("commodity-list");
+	var left=document.getElementById("left");
+	var right=document.getElementById("right");
+
+   	function animate (dd){
+   	content.style.left = parseInt(content.style.left) + dd + "px";
+   
+   if(content.style.left > -270){
+   		content.style.left = -810 + 'px';
+   	}
+   
+   if(content.style.left < -810){
+   		content.style.left = -270 + 'px';
+   	}
+   }
+   
+   	right.onclick = function(){
+   	
+   	animate(-270);
+   }
+	left.onclick = function () {
+	animate(270);
+}
+
+
